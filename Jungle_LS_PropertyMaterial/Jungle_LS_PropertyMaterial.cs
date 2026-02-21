ce0f767df4dad5f2d03c65083dab57c0644f95e1
Алексей Согрин <lebron23ny@yandex.ru>
2026-02-21 12:59:37 +0300

    Добавьте файлы проекта.

---
+++ Jungle_LS_PropertyMaterial/Jungle_LS_PropertyMaterial.cs
@@ -0,0 +1,58 @@
+﻿using FEMaterial;
+using FEModel;
+using FEModel.Element;
+using FEModel.Interfaces;
+using LiraAPI;
+using System.Windows.Forms;
+
+namespace Jungle_LS_PropertyMaterial
+{
+    public class Jungle_LS_PropertyMaterial : ILiraPrimeAPI
+    {
+        public ReturnCodes ExecuteProgram_Prime(IModelAPI pModelAPI, Results_Key pCurentCase)
+        {
+            IModel imodel = pModelAPI as IModel;
+            if (imodel == null) // модель не определена
+                return ReturnCodes.RC_IGNOR;
+
+            int numberMaterial = -1;
+            int numbEl = imodel.getAllElementNumber();
+            for (int i = 0; i < numbEl; i++)
+            {
+                CBar cBar = imodel.getElement(i) as CBar;
+                if (cBar != null)
+                {
+                    if (cBar.getFlag(IElement.e_ElFlag.EEF_Selected))
+                    {
+                        numberMaterial = cBar.getMaterial();
+                        break;
+                    }
+                }
+            }
+            if (numberMaterial == -1)
+            {
+                MessageBox.Show("Выберите стрежень с назначенным материалом");
+
+
+                return ReturnCodes.RC_IGNOR;
+            }
+
+            IMaterialContainer matContainer = imodel.getMaterialContainer();
+            IMaterial material = matContainer.getMaterial(numberMaterial);
+            if (material is MaterialDBSteelRoll stMaterial)
+            {
+                string tableType = stMaterial.getTableType().ToString();
+                string tableName = stMaterial.getTableName().ToString();
+                string materialType = stMaterial.getMaterialClass();
+                MessageBox.Show($"Номер материала: {numberMaterial}\n{tableType}\n{tableName}\n{materialType}");
+                Clipboard.SetText($"Номер материала: {numberMaterial}\n{tableType}\n{tableName}\n{materialType}");
+                return ReturnCodes.RC_OK;
+            }
+            else
+            {
+                MessageBox.Show("У выбранного стержня не стальной материал");
+                return ReturnCodes.RC_IGNOR;
+            }
+        }
+    }
+}
---
+++ Jungle_LS_PropertySection/Plugin_PropertySection.cs
@@ -0,0 +1,60 @@
+﻿using FEModel;
+using FEModel.Element;
+using FEModel.Interfaces;
+using FESection;
+using LiraAPI;
+using System.Windows.Forms;
+
+namespace Jungle_LS_PropertySection
+{
+    public class Plugin_PropertySection : ILiraPrimeAPI
+    {
+        public ReturnCodes ExecuteProgram_Prime(IModelAPI pModelAPI, Results_Key pCurentCase)
+        {
+            IModel imodel = pModelAPI as IModel;
+            if (imodel == null) // модель не определена
+                return ReturnCodes.RC_IGNOR;
+
+
+            int numberSection = -1;
+            int numbEl = imodel.getAllElementNumber();
+            for (int i = 0; i < numbEl; i++)
+            {
+                CBar cBar = imodel.getElement(i) as CBar;
+                if (cBar != null)
+                {
+                    if (cBar.getFlag(IElement.e_ElFlag.EEF_Selected))
+                    {
+                        numberSection = cBar.getSection();
+                        break;
+                    }
+                }
+            }
+            if (numberSection == -1)
+            {
+                MessageBox.Show("Выберите стрежень с назначенным сечением");
+                return ReturnCodes.RC_IGNOR;
+            }
+
+            ISectionContainer sectionContainer = imodel.getSectionContainer();
+            ISection section = sectionContainer.getSection(numberSection);
+            if (section is StBaseSection stSection)
+            {
+                string sectionName = stSection.get_ShapeName();
+                string sectType = stSection.getSectionType().ToString();
+                string tableTyle = stSection.getTableType().ToString();
+                string tableFileName = stSection.get_TableFileName();
+
+                MessageBox.Show($"Номер сечения: {numberSection}\n{sectionName}\n{sectType}\n{tableTyle}\n{tableFileName}");
+                Clipboard.SetText($"{sectionName}\n{sectType}\n{tableTyle}\n{tableFileName}");
+                return ReturnCodes.RC_OK;
+
+            }
+            else
+            {
+                MessageBox.Show("У выбранного стержня не стальное сечение");
+                return ReturnCodes.RC_IGNOR;
+            }            
+        }
+    }
+}
